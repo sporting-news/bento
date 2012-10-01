a6bfce4eb3b08004b7991ef1ee0628c00379a0e4
jcopp <jcopp@sportingnews.com>
2012-10-01 15:08:47 -0400

    Initial add of SN definition for CentOS 5.8

---
+++ definitions/sn-centos-5.8/definition.rb
@@ -0,0 +1,11 @@
+require File.dirname(__FILE__) + "/../.centos/session.rb"
+
+iso = "CentOS-5.8-x86_64-netinstall.iso"
+
+session =
+  CENTOS_SESSION.merge({ :os_type_id => 'RedHat',
+                         :iso_file => iso,
+                         :iso_md5 => "6425035e9adee4b8653a85f59877ac5b",
+                         :iso_src => "http://mirror.yellowfiber.net/centos/5.8/isos/x86_64/#{iso}" })
+
+Veewee::Session.declare session
