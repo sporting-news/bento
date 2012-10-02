c0a0f313e2a0b58090f0c6a336518c68614c9617
jcopp <jcopp@sportingnews.com>
2012-10-02 17:06:21 -0400

    Remove i?86 packages

--- definitions/sn-centos-5.8/cleanup.sh
+++ definitions/sn-centos-5.8/cleanup.sh
@@ -1,1 +1,7 @@
-../.centos/cleanup.sh
+#!/bin/bash -eux
+yum -y erase gtk2 libX11 hicolor-icon-theme avahi freetype bitstream-vera-fonts
+yum -y erase *.i?86
+yum -y clean all
+rm -rf VBoxGuestAdditions_*.iso VBoxGuestAdditions_*.iso.?
+rm -f /tmp/chef*rpm
+
