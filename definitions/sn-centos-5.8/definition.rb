8356f48fb15f999c7dc8a756bfc77cb22fb2cad3
jcopp <jcopp@sportingnews.com>
2012-11-07 09:59:08 -0500

    Clean up string formatting

--- definitions/sn-centos-5.8/definition.rb
+++ definitions/sn-centos-5.8/definition.rb
@@ -1,10 +1,11 @@
 require File.dirname(__FILE__) + "/../.centos/session.rb"
 
 iso = "CentOS-5.8-x86_64-netinstall.iso"
+iso_url = "http://mirror.yellowfiber.net/centos/5.8/isos/x86_64/#{iso}"
 
 session =
   CENTOS_SESSION.merge({ :iso_file => iso,
                          :iso_md5 => "6425035e9adee4b8653a85f59877ac5b",
-                         :iso_src => "http://mirror.yellowfiber.net/centos/5.8/isos/x86_64/#{iso}" })
+                         :iso_src => "#{iso_url}" })
 
 Veewee::Session.declare session
